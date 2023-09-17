bb3bde4c05aa40f7bd9de9fe0b0d9d265817a299
hansanaCham <hansanacham@gmail.com>
2023-09-17 15:34:13 +0530

    ok

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:task_manager/providers/task_provider.dart';
 
 import '../widgets/appbar.dart';
 import '../widgets/task_list.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   Home({super.key});
@@ -29,8 +31,25 @@ class _HomeState extends State<Home> {
     );
   }
 
-  _addTask() => Navigator.pushNamed(
-        context,
-        '/addTask',
-      );
+  _addTask() async {
+    await dotenv.load(fileName: ".env");
+    final apiKey = dotenv.env['API_KEY']!;
+    print(apiKey);
+    final url = Uri.parse(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=$apiKey');
+    final body = {
+      'email': 'hansanacham@gmail.com',
+      'password': 'teatime',
+      'returnSecureToken': true,
+    };
+    final response = await http.post(url, body: body);
+    if (response.statusCode != 200) {
+      throw Exception('Failed to sign in');
+    }
+  }
+
+//   _addTask() => Navigator.pushNamed(
+//         context,
+//         '/addTask',
+//       );
 }
